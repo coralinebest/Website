244dafccb442acf7dadec6f46701d3ac654af531
coralinebest <147934598+coralinebest@users.noreply.github.com>
2024-11-10 00:06:08 +0100

    Dernières modifications apportées au site web

--- send_email.php
+++ send_email.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars(trim($_POST['message']));
 
     // Set your email address where you want to receive messages
-    $to = "mboccardi71@gmail.com";
+    $to = "cocojudith.best@gmail.com";
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
